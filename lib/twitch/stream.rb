c500040db0e43948d3f4a790d9e8abd248b96b6e
Maurice Wahba <maurice.wahba@gmail.com>
2017-11-06 20:19:41 -0500

    Date parsing

--- lib/twitch/stream.rb
+++ lib/twitch/stream.rb
@@ -1,10 +1,17 @@
+require "time"
+
 module Twitch
   class Stream
     attr_reader :id, :user_id, :game_id, :community_ids, :type, :title, :viewer_count, :started_at, :language, :thumbnail_url
-    
+    DATE_ATTRIBUTES = [:started_at]
+
     def initialize(attributes = {})
       attributes.each do |k, v|
-        instance_variable_set("@#{k}", v)
+        if DATE_ATTRIBUTES.include?(k.to_sym)
+          instance_variable_set("@#{k}", Time.parse(v))
+        else
+          instance_variable_set("@#{k}", v)
+        end
       end
     end
   end
--- lib/twitch/video.rb
+++ lib/twitch/video.rb
@@ -2,11 +2,17 @@ require "time"
 
 module Twitch
   class Video
+    DATE_ATTRIBUTES = [:created_at, :published_at]
+
     attr_reader :id, :title, :description, :language, :view_count, :created_at, :published_at, :thumbnail_url
 
     def initialize(attributes = {})
       attributes.each do |k, v|
-        instance_variable_set("@#{k}", v)
+        if DATE_ATTRIBUTES.include?(k.to_sym)
+          instance_variable_set("@#{k}", Time.parse(v))
+        else
+          instance_variable_set("@#{k}", v)
+        end
       end
     end
   end
