612cddd3119b9f2d23d27fd3ef89ae12f5dc6467
Maurice Wahba <maurice@web.local.tournup.com>
2018-12-22 11:13:44 -0500

    Add user name to other data models

--- lib/twitch/user_follow.rb
+++ lib/twitch/user_follow.rb
@@ -3,14 +3,20 @@ module Twitch
   class UserFollow
     # User ID of the *following* user.
     attr_reader :from_id
+    # Display name of the *following* user.
+    attr_reader :from_name
     # User ID of the *followed* user.
     attr_reader :to_id
+    # User name of the *followed* user.
+    attr_reader :to_name
     # Date at which the follow action was performed.
     attr_reader :followed_at
 
     def initialize(attributes = {})
       @from_id = attributes['from_id']
+      @from_name = attributes['from_name']
       @to_id = attributes['to_id']
+      @to_name = attributes['to_name']
       @followed_at = Time.iso8601(attributes['followed_at'])
     end
   end
